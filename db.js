ce5579bedf31001e318e6cfd035750c11b984642
Gisell Mazo <gisellmazo@gmail.com>
2020-12-24 18:22:30 -0500

    cambio en api

--- db.js
+++ db.js
@@ -9,14 +9,14 @@ module.exports = () => {
                 "correo" : "gisellmazo@gmail.com",
                 "usuario" : "gisellmazo13",
                 "contraseña" : "1214743656",
-                "tareas" : [
+                "tareas" : 
                     {
                         "tarea1" : "Hacer tarea de Historia",
                         "tarea2" : "Estudiar Inglés",
                         "tarea3" : "Ejercitarme",
                         "tarea4" : "Leer libro pendiente"
                     }
-                ],
+                
                 
             }
         ]
